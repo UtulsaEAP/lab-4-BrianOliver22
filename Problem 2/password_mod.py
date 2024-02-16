22ea21dc6badff5b6b6777f90bea5aa3cc6c7c9d
Jacob Oliver <bjo8656@utulsa.edu>
2024-02-16 16:29:41 -0600

    gggggg

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -15,9 +15,8 @@ def password_mod():
     password= password.replace("m","M")
     password = password.replace("B","8")
     password= password.replace("s","$")
-    password = [password,"!"]
-    fuck_you = " "
-    password = fuck_you.join(password)
+    password = password+"!"
+  #  password = fuck_you.join(password)
 
     print(password)
 
