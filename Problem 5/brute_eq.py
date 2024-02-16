c69f4ff343cde66807a50f01aee883c329d6dc9c
Jacob Oliver <bjo8656@utulsa.edu>
2024-02-16 15:58:39 -0600

    q123234

--- Problem 5/brute_eq.py
+++ Problem 5/brute_eq.py
@@ -18,8 +18,8 @@ def brute_eq():
     e = int(input())
     f = int(input())
     check = False
-    for x in range(-10, 10, 1):
-        for y in range(-10, 10, 1):
+    for x in range(-10, 11, 1):
+        for y in range(-10, 11, 1):
             if a * x + b * y == c and d * x + e * y == f:
                check = True
                print("x=",x, ", y=",y)
